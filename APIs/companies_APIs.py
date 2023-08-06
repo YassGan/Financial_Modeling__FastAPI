a883fbcab84686fd032b15b88f190afa48e4c498
YassGan <yassinegana.gana@gmail.com>
2023-08-06 15:04:35 +0200

    Finishing the work of creatino of the companies, now we should work on calling the necessary functions in the scheduler

--- APIs/companies_APIs.py
+++ APIs/companies_APIs.py
@@ -6,6 +6,7 @@ from models.Sector import Sector
 from config.db import get_database 
 
 from APIs.sectors_APIs import find_sector_id_by_name
+from APIs.exchange_APIs import find_Exchange_id_by_name
 
 from APIs.countries_APIs import find_Country_id_by_name  
 from APIs.countries_APIs import find_subregion_id_by_Countryname  
@@ -51,12 +52,21 @@ def getCompanyBySymbol(Symbol: str):
     return data
 
 # Function that returns the peers of a company
-def CompanyPeers(Symbol):
-    api_url = f"https://financialmodelingprep.com/api/v4/stock_peers?symbol={Symbol}&apikey={api_key}"
-    print(api_url)
-    response = requests.get(api_url)
-    return response.json()
+def CompanyPeers(symbol):
 
+    api_url = f"https://financialmodelingprep.com/api/v4/stock_peers?symbol={symbol}&apikey={api_key}"
+
+    response = requests.get(api_url)
+    
+    # Check if response contains peers
+    if response.json() and 'peersList' in response.json()[0]:
+        return response.json()[0]['peersList']
+    
+    # Return custom message if no peers
+    else:
+        return 'No peers companies'
+    
+    
 # API that launches the function companyPerrs
 @Company.get("/getCompanyPeers/{Symbol}")
 def getCompanyPeersBySymbol(Symbol: str):
@@ -105,82 +115,46 @@ def creatingCompanies():
 
 
     
-    CompaniesToInsertInDatabase=[]
 
     for i in range(len(companiesSymbolsList)-1):
-        CompanyInfo_fromAPI=fetch_data_from_api_bySymbol(companiesSymbolsList[i]['Symbol'])[0]
-
-        print("Treating the company ",companiesSymbolsList[i]['Symbol'])
-        CompaniesToInsertInDatabase.append({
-                        "sectorId": find_sector_id_by_name(CompanyInfo_fromAPI['sector']),
-                        "subregionId": find_subregion_id_by_Countryname(CompanyInfo_fromAPI['country']),
-                        "countryId": find_Country_id_by_name(CompanyInfo_fromAPI['country']),
-                        "companyName":CompanyInfo_fromAPI['companyName'],
-                        "symbol":companiesSymbolsList[i]['Symbol'],
-                        "ipoDate":CompanyInfo_fromAPI['ipoDate'],
-                        "isin":CompanyInfo_fromAPI['isin'],
-                        "exchange":CompanyInfo_fromAPI['exchange'],
-                        "exchangeShortName":CompanyInfo_fromAPI['exchangeShortName'],
-                        "industry":CompanyInfo_fromAPI['industry'],
-                        "sector":CompanyInfo_fromAPI['sector'],
-                        "website":CompanyInfo_fromAPI['website'],
-                        "description":CompanyInfo_fromAPI['description'],
-                        "image":CompanyInfo_fromAPI['image'],
-                        # "peers":CompanyPeers( CompanyInfo_fromAPI['image'])[0]['peersList'])
-
-                    })
+        symbol = companiesSymbolsList[i]['Symbol']
+        existing_company = get_companies_collection().find_one({"symbol": symbol})
         
-    CompaniesToInsertInDatabaseDf = pd.DataFrame(CompaniesToInsertInDatabase)
-    
+        if existing_company:
+            print("------- >  Company with symbol", symbol, "already exists ")
+        else:
+            CompanyInfo_fromAPI = fetch_data_from_api_bySymbol(symbol)[0]
 
-        
-     
-    print("CompanyData List ")
-    print(CompaniesToInsertInDatabase)
+            print("Treating the company ", symbol)
+            get_companies_collection().insert_one({
+                "sectorId": find_sector_id_by_name(CompanyInfo_fromAPI['sector']),
+                "subregionId": find_subregion_id_by_Countryname(CompanyInfo_fromAPI['country']),
+                "countryId": find_Country_id_by_name(CompanyInfo_fromAPI['country']),
+                "exchangeId": find_Exchange_id_by_name(CompanyInfo_fromAPI['exchangeShortName']),
 
-    print("CompanyData df ")
-    print(CompaniesToInsertInDatabaseDf)
-
-
-    if not CompaniesToInsertInDatabaseDf.empty:
-            new_companies = CompaniesToInsertInDatabaseDf.to_dict(orient='records')
-            
-            existing_companies = set(get_companies_collection().distinct("symbol"))
-            new_companies_to_create = [company for company in new_companies if company.get("symbol") not in existing_companies]
-
-            if new_companies_to_create:
-                get_companies_collection().insert_many(new_companies_to_create)
-                print (f"- {len(new_companies_to_create)} new new_companies created successfully.")
-            else:
-                print( "- No new new_companies to create.")
-    else:
-            print( "- No new new_companies to create.")
-
-
-
-
-    # print("Company sectorId ", find_sector_id_by_name(CompanyInfo_fromAPI['sector']))
-    # print("Company subregionId ", find_subregion_id_by_Countryname(CompanyInfo_fromAPI['country']))
-    # print("Company countryId ", find_Country_id_by_name(CompanyInfo_fromAPI['country']))
-
-    # print("Company name ",CompanyInfo_fromAPI['companyName'])
-    # print("Company symbol ",companiesSymbolsList[0]['Symbol'])
-    # print("Company ipoDate ",CompanyInfo_fromAPI['ipoDate'])
-    # print("Company isin ",CompanyInfo_fromAPI['isin'])
-    # print("Company exchange ",CompanyInfo_fromAPI['exchange'])
-    # print("Company exchangeShortName ",CompanyInfo_fromAPI['exchangeShortName'])
-    # print("Company industry ",CompanyInfo_fromAPI['industry'])
-    # print("Company sector ",CompanyInfo_fromAPI['sector'])
-    # print("Company website ",CompanyInfo_fromAPI['website'])
-    # print("Company description ",CompanyInfo_fromAPI['description'])
-    # print("Company image ",CompanyInfo_fromAPI['image'])
-    # print("Company peers ",CompanyPeers(companiesSymbolsList[0]['Symbol'])[0]['peersList'])
+                "companyName": CompanyInfo_fromAPI['companyName'],
+                "symbol": symbol,
+                "ipoDate": CompanyInfo_fromAPI['ipoDate'],
+                "isin": CompanyInfo_fromAPI['isin'],
+                "exchange": CompanyInfo_fromAPI['exchange'],
+                "exchangeShortName": CompanyInfo_fromAPI['exchangeShortName'],
+                "industry": CompanyInfo_fromAPI['industry'],
+                "sector": CompanyInfo_fromAPI['sector'],
+                "website": CompanyInfo_fromAPI['website'],
+                "description": CompanyInfo_fromAPI['description'],
+                "country": CompanyInfo_fromAPI['country'],
+                "image": CompanyInfo_fromAPI['image'],
+                "peers": CompanyPeers(CompanyInfo_fromAPI['symbol'])
+            })
 
 
-    # print("Company price ",CompanyInfo_fromAPI['price'])
 
 
-    return len(DataFrame)
+
+
+       
+
+
 
 
 
