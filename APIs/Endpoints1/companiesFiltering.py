72840a3f5ee6fa0248c49fd5a210817a59d917fc
YassGan <yassinegana.gana@gmail.com>
2023-08-30 21:23:29 +0200

    In this code i added the filter with many elements in other words we can make a filtering with many countries, and many sectors ... also i added filtering with multiple keywords

--- APIs/Endpoints1/companiesFiltering.py
+++ APIs/Endpoints1/companiesFiltering.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter,Response,Query
 
 from config.db import get_database 
 
- 
+import pymongo 
 CompanyFiltering=APIRouter()
 
 
@@ -21,6 +21,15 @@ def get_companies_collection():
 
 CompaniesCollection=get_companies_collection()
 
+# Create an index on multiple fields
+CompaniesCollection.create_index([
+    ("description", "text"),
+    ("companyName", pymongo.ASCENDING),  
+    ("sector", pymongo.ASCENDING),
+    ("industry", pymongo.ASCENDING),
+    ("subregion", pymongo.ASCENDING),
+    ("country", pymongo.ASCENDING)
+])
 
 
 from bson.json_util import dumps
@@ -28,48 +37,58 @@ from bson.json_util import dumps
 # Example on how to use this api 
 # http://localhost:8000/filterCompanies?name=CompanyName&sector=Technology&industry=Software&subregion=North&country=USA&keywords=AI"
 
-#Curl of many countries http://localhost:1001/filterCompanies_Filtering?country=FR&country=DK&country=SE
+#Curl of many fields for example countries http://localhost:1001/filterCompanies_Filtering?country=FR&country=DK&country=SE
+
+import re
 
 @CompanyFiltering.get("/filterCompanies_Filtering")
 async def filter_companies(name: str = Query(None, title="Company Name"),
-                           sector: str = Query(None, title="Sector"),
-                           industry: str = Query(None, title="Industry"),
-                           subregion: str = Query(None, title="Subregion"),
+                            sector: List[str] = Query(None, title="Sector"),
+                            industry: List[str] = Query(None, title="Industry"),
+                            subregion: List[str] = Query(None, title="Subregion"),
                             country: List[str] = Query(None, title="Country"),
-                           keywords: str = Query(None, title="Keywords")):
+                            keywords: List[str] = Query(None, title="Keywords")):
+
     filters = {}
 
     if name:
         filters["companyName"] = name
 
     if sector:
-        filters["sector"] = sector
+        filters["sector"] = {"$in": sector}
 
     if industry:
-        filters["industry"] = industry
+        filters["industry"] = {"$in": industry}
 
     if subregion:
-        filters["subregion"] = subregion
+        filters["subregion"] = {"$in": subregion}
 
     if country:
         filters["country"] = {"$in": country}
 
     if keywords:
-        filters["description"] = {"$regex": f".*{keywords}.*", "$options": "i"}
+        search_string = " ".join(keywords)
+        filters["$text"] = {"$search": search_string}
 
-    projection = {"_id": 0, "companyName": 1, "sector": 1, "industry": 1}  # Add other fields you need
+
+    projection = {"_id": 0, "companyName": 1, "sector": 1, "industry": 1, "description": 1}
 
     try:
-        companies_collection = get_companies_collection()
-        filtered_companies = companies_collection.find(filters, projection)
-
-        # Limit the number of returned results using .limit() if needed
-        # filtered_companies = filtered_companies.limit(10)
+        filtered_companies = CompaniesCollection.find(filters, projection)
+        filtered_companies = CompaniesCollection.find(filters, projection)
+        explanation = filtered_companies.explain()
+        print(">>>>>>>>> explanation")
+        print(explanation)
 
         return list(filtered_companies)
 
     except Exception as e:
         return {"error": str(e)}
+    
+
+
+
+
 
 
 @CompanyFiltering.get("/autocompleteCompanyName")
