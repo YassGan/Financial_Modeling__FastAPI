ac2e37a4b30757b309da1a7aaad32489a7953ea0
YassGan <yassinegana.gana@gmail.com>
2023-10-01 22:01:15 +0200

    Adding the LTM functionality to return financial information data, for the cash flow and the income statement

--- APIs/Endpoints2/Get_Financial_Information.py
+++ APIs/Endpoints2/Get_Financial_Information.py
@@ -87,6 +87,69 @@ IncomeStatementQuarterCollection=get_IncomeStatementQuarter_collection()
 
 
 
+
+import calendar
+
+def LTM_date_adjuster(date_str):
+    try:
+        # Convert the input date string to a datetime object
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+
+        
+        year = date_obj.year
+        month = date_obj.month
+
+        print(f"The extracted month of the entered date is  {month}  and its type is {type(month)}")
+
+
+
+        if(month>=1 and month<=3):
+            month=12
+            year=year-1
+
+        elif(month>3 and month<6):
+            month=3
+
+        elif(month>=6 and month<9):
+            month=6
+
+        elif(month>=9 and month<=12):
+            month=9
+        
+
+        
+        _, last_day = calendar.monthrange(year, month)
+        
+        last_day_date_str = f"{year}-{month:02d}-{last_day:02d}"
+
+        print(">>>> The entered date ",date_str)
+        print(">>>> The adjusted date ",last_day_date_str)
+
+        
+        return last_day_date_str
+    except ValueError:
+        return "Invalid date format. Please use dd-mm-yyyy."
+
+
+
+
+def get_previous_year_date(input_date_str):
+    try:
+        year, month, day = input_date_str.split("-")
+        
+        previous_year = int(year) - 1
+        
+        previous_year_date_str = f"{previous_year}-{month}-{day}"
+        
+        return previous_year_date_str
+    except ValueError:
+        return "Invalid date format. Please use YYYY-MM-DD."
+
+
+
+
+
+
 @Get_Financial_Info.get('/AllBS')
 async def find_all_sectors():
     return serializeList2(BalanceSheetAnnualCollection.find())
@@ -139,8 +202,32 @@ def get_Financials_Data(symbol,start_date, end_date,maxElem,StatementType,Freque
 
         if StatementType=="PL" and Frequency=="Q":
             Collection=IncomeStatementQuarterCollection
+            
 
 
+# LTM example, the company should have an income statement at the given date otherwise it will return an empty array 
+#http://localhost:1001/financials?symbol=AY&start_date_str=2015-04-05&limit=4&StatementType=PL&Frequency=LTM
+        if StatementType=="PL" and Frequency=="LTM":
+            Collection=IncomeStatementQuarterCollection
+
+            # print("Before ")
+            # print("start_date ",start_date)
+            # print("Before ")
+
+            adjusted_LTM_start_date=LTM_date_adjuster(start_date)
+            start_date=adjusted_LTM_start_date
+
+            last_Year_LTM_start_date=get_previous_year_date(start_date)
+            end_date=last_Year_LTM_start_date
+            Collection=IncomeStatementQuarterCollection
+
+            aux=start_date
+            start_date=end_date
+            end_date=aux
+
+
+            print("Starting date ",start_date)
+            print("Ending date ",end_date)
 
 
 
@@ -152,16 +239,16 @@ def get_Financials_Data(symbol,start_date, end_date,maxElem,StatementType,Freque
             Collection=CashFlowQuarterCollection
 
 
-
-
+        print("")
+        print("")
+        print(">>> Starting the treatement of collection.find")
+        print(f" start_date {start_date} / the end_date {end_date} / the collection {Collection}")
         all_BalanceSheetAnnual = Collection.find({
             "$and": [
                 {"date": {"$gte": start_date, "$lte": end_date}},
                 {"symbol": symbol}  
             ]
         }).sort("date", 1).limit(int_maxElem)
-
-
 
         if not all_BalanceSheetAnnual:
             return []
@@ -182,7 +269,7 @@ from datetime import datetime
 
 
 #A curl example to this api 
-#http://localhost:1001/get_all_BalanceSheetAnnual?symbol=GOOS.TO&start_date_str=2018-01-05&end_date_str=2022-01-05&limit=10&StatementType=PL&Frequency=Q
+#http://localhost:1001/financials?symbol=GOOS.TO&start_date_str=2018-01-05&end_date_str=2022-01-05&limit=10&StatementType=PL&Frequency=Q
 @Get_Financial_Info.get('/financials')
 def get_financials_API(
     symbol: str = Query(None, title="symbol"),
@@ -196,7 +283,13 @@ def get_financials_API(
 ):
     try:
 
-
         return get_Financials_Data(symbol,start_date_str, end_date_str,limit,StatementType,Frequency)
     except Exception as e:
         raise e
+    
+
+@Get_Financial_Info.get("/date_LTM_Adjuster_tester/{entered_Date}")
+def date_LTM_Adjuster_tester_API(entered_Date: str):
+    adjustedDate = LTM_date_adjuster(entered_Date)
+    last_Year=get_previous_year_date(adjustedDate)
+    return {"adjustedDate":adjustedDate,"last_Year":last_Year}
